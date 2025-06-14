f42dd5fae05a34ffe69631c388ea0fb5813b8e24
asavy79 <asavard@uoguelph.ca>
2025-06-13 22:00:02 -0700

    added vectorized operations for rsi

--- src/indicators.py
+++ src/indicators.py
@@ -53,41 +53,29 @@ class RSI(Indicator):
         return self.period
 
     def get_rsi_values(self, differences):
-        l = 1
-
-        differences = np.array(differences)
-
-        pos_changes, neg_changes = 0.0, 0.0
-
         period = self.get_period()
 
-        arr = []
+        differences = differences.dropna()
 
-        for r in range(1, len(differences)):
-            print(differences[r])
-            if differences[r] > 0:
-                pos_changes += abs(differences[r])
-            else:
-                neg_changes += abs(differences[r])
-            if (r - l)+1 == period:
-                avg_loss, avg_gain = float(
-                    neg_changes) / period, float(pos_changes) / period
-                if avg_loss == 0:
-                    rsi = 100.00
-                else:
-                    rs = avg_gain / avg_loss
-                    rsi = 100 - (100 / (1 + rs))
-                arr.append(rsi)
-                if differences[l] < 0:
-                    neg_changes -= abs(differences[l])
-                else:
-                    pos_changes -= abs(differences[l])
-                l += 1
-        return np.array(arr)
+        gains = differences.clip(lower=0)
+        losses = -differences.clip(upper=0)
+
+        avg_gain = gains.ewm(alpha=1/period, adjust=False).mean()
+        avg_loss = losses.ewm(alpha=1/period, adjust=False).mean()
+
+        rs = avg_gain / avg_loss
+
+        # handle edge case where the stock has only gone up in the time period
+        rs = rs.replace([np.inf, -np.inf], np.nan).fillna(0)
+
+        rsi = 100 - (100 / (1+rs))
+        return rsi
 
 
-rsi_14 = RSI(14)
+indicator = RSI(period=14)
+
 
 stock = Stock("AAPL", 100)
+mas = indicator.compute(stock)
 
-rsi_vals = rsi_14.compute(stock)
+print(mas)
