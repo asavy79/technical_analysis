94d30126bb8d8a017d5a0ae217c250a3d58c360a
asavy79 <asavard@uoguelph.ca>
2025-07-19 17:19:45 -0700

    configured fastapi

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+import logging
+
+
+app = FastAPI()
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    logger.info("Hello from the main api route!")
+    return {
+        "message": f"Welcome to the backtesting engine!",
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
